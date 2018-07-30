23ebc82873dbe518eb37115c7e8c714d810e5e3b
Chen Wang <cwang138@illinois.edu>
2018-07-30 13:15:52 -0500

    change static documentation page to json rendered page

--- routes/documentation.js
+++ routes/documentation.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var appPath = path.dirname(__dirname);
+var documentation = require(path.join(appPath, 'public', 'documentation.json'));
 
 router.get('/documentation', function(req, res, next){
-    res.render('documentation',{});
+    res.render('documentation', documentation);
 });
 
 module.exports = router;
